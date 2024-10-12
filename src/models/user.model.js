3ac6fc6be3ec1dc83749c3a028a90e938d37d4cb
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-10-12 22:01:24 +0530

    modified user schema to store unsolved questions

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,6 +22,10 @@ const userSchema = new Schema({
     type: String,
     required: true, // This will store the Firebase UID
   },
+  unsolvedQuestions: {
+    type: [String], // or [Schema.Types.ObjectId] if referencing another schema
+    default: [], // Initializes with an empty array
+  },
 });
 
 export const User = mongoose.model("User", userSchema);
